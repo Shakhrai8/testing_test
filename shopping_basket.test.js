dd51b76c09f4f522544b5afd2f229323a81ced98
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-13 15:03:11 +0100

    debuging challenge

--- shopping_basket.test.js
+++ shopping_basket.test.js
@@ -3,18 +3,10 @@ const ShoppingBasket = require ('./shopping_basket');
 
 describe('moscking tests', () =>{
   it ('works', () =>{
-    const CandyDouble = { getName: () => "fake", getPrice: () => 9.99 };
+    const CandyDouble = { getPrice: () => 10 };
     const new_basket = new ShoppingBasket;
     new_basket.addItem(CandyDouble);
-    expect(new_basket.getTotalPrice()).toBe(9.99);
-  });
-
-  it ('works', () =>{
-    const CandyDouble = { getName: () => "fake", getPrice: () => 10.00 };
-    const CandyDouble2 = { getName: () => "fake2", getPrice: () => 10.00 };
-    const new_basket = new ShoppingBasket;
-    new_basket.addItem(CandyDouble);
-    new_basket.addItem(CandyDouble2);
-    expect(new_basket.getTotalPrice()).toBe(20);
+    new_basket.applyDiscount(6);
+    expect(new_basket.getTotalPrice()).toBe(4);
   });
 });
